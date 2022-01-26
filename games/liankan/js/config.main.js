3a77158e18e84f06ed499e819b28cc3e6c0a9313
Ramsayi <3197293846@qq.com>
2022-01-26 22:33:40 +0800

    删除无用文件

--- games/liankan/js/config.main.js
+++
@@ -1,2 +0,0 @@
-dataForWeixin.appId = "wx8820cdf5db680ffa";
-dataForWeixin.url = "http://g.lanrenmb.com/"+_con["num"]+"/";
--- games/xiaoshuai/js/7/count.js
+++
@@ -1,9 +0,0 @@
-function openCount(id){
-	$.getJSON(_config["isCount"]+"count/count.php?callback=?", {id: id, type: 1}, function(data){});
-}
-function clickCount(id){
-	$.getJSON(_config["isCount"]+"count/count.php?callback=?", {id: id, type: 2}, function(data){});		
-}
-function shareCount(id){
-	$.getJSON(_config["isCount"]+"count/count.php?callback=?", {id: id, type: 3}, function(data){});		
-}
